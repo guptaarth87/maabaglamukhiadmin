63499f3a46f4157d08326f312b89da612348ab98
guptaarth87 <guptaarth87@gmail.com>
2024-03-12 11:50:53 +0530

    frontend

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,15 @@ import {Route , Routes} from 'react-router-dom'
 import CategoryPage from './Pages/CategoryPage'
 import MenuPage from './Pages/MenuPage'
 import SubCategoryPage from './Pages/SubCategoryPage'
+import LoginPage from './Pages/LoginPage'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
     <Routes>
-      <Route path='/' element={<CategoryPage/>}/>
+      <Route path='/' element={<LoginPage/>}/>
+            <Route path='/category' element={<CategoryPage/>}/>
       <Route path='/menu' element={<MenuPage/>}/>
       <Route path='/subcategories' element={<SubCategoryPage/>}/>
     </Routes>
